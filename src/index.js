5315bb4e0a397c66802089d9d2066ec8bd0bbf03
sintornillos <rubiancocarlos@gmail.com>
2022-04-11 18:33:38 -0300

    Inicio de graphql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 // const express = require('express');
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
+const schema = {};
+
+app.get('/', (req, res) => {
+  res.json('Hello world');
+});
+
+app.use('/grp', graphqlHTTP({
+  graphiql: true,
+  schema: schema
+}));
 
 app.listen(3000, () => console.log(`Server on port 3000`));
